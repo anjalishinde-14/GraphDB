0c4745286142fa91a37bf15cc1410d29c5a94c15
Anjali Shinde <anjalishinde14022004@gmail.com>
2025-08-14 21:51:11 +0530

    added findeNode method

--- src/graph.ts
+++ src/graph.ts
@@ -19,9 +19,20 @@ class Graph{
         })
         return nodeId;
     }
+    findNodes(key:string, value:string):any[]{
+        const result: any[]=[];
+        for(let [x,node] of this.nodes){
+            if(node.properties[key]==value){
+                result.push(node);
+            }
+        }
+        return result;
+    }
 }
 
 let gh = new Graph();
 let node = gh.addNode({'name' : 'Pookie'});
-console.log(gh.nodes.get('0').properties);
+//console.log(gh.nodes.get('0').properties);
 //console.log(gh);
+let result: any[] = gh.findNodes("name","Pookie");
+console.log(result);
