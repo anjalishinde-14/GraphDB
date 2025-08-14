61a73d0d0181bd9e469efb2966c991bf396c5ec9
Anjali Shinde <anjalishinde14022004@gmail.com>
2025-08-14 21:39:18 +0530

    Graph class and addNode method

--- src/graph.ts
+++ src/graph.ts
@@ -1,4 +1,27 @@
-function adds(a : number ,b : number) : number {
-    return a+b;
+class Graph{
+    nodes: Map<string,any>;
+    edges: Map<string,any>;
+    nodeIdCounter: number;
+    edgeIdCounter: number;
+    constructor(){
+        this.nodes= new Map;
+        this.edges= new Map;
+        this.nodeIdCounter=0;
+        this.edgeIdCounter=0;
+    }
+    addNode(properties:{}):string{
+        const nodeId = (this.nodeIdCounter++).toString();
+        this.nodes.set(nodeId,{
+            id:nodeId,
+            properties,
+            inedges: new Set(),
+            outedges: new Set()
+        })
+        return nodeId;
+    }
 }
-console.log(adds(36,2));
+
+let gh = new Graph();
+let node = gh.addNode({'name' : 'Pookie'});
+console.log(gh.nodes.get('0').properties);
+//console.log(gh);
